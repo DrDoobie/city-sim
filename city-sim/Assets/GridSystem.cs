ce886c9b0741045cc97cf421c97cd12a94bf6963
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-06-27 22:51:25 +0200

    Started on building system

---
+++ city-sim/Assets/GridSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour {
+
+	public float gridSize;
+	public GameObject target;
+	public GameObject prefab;
+	Vector3 truePos;
+ 
+	private void LateUpdate () {
+		truePos.x = (Mathf.Floor(target.transform.position.x / gridSize) * gridSize);
+		truePos.y = (Mathf.Floor(target.transform.position.y / gridSize) * gridSize);
+		truePos.z = (Mathf.Floor(target.transform.position.z / gridSize) * gridSize);
+
+		prefab.transform.position = truePos;
+	}
+}
