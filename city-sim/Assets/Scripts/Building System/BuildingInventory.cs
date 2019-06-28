6e58716c9d3f2c04fed90ca9bc9279e93f6f78b9
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-06-28 03:14:54 +0200

    Inventory ui done

--- city-sim/Assets/Scripts/Building System/BuildingInventory.cs
+++ city-sim/Assets/Scripts/Building System/BuildingInventory.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class BuildingInventory : MonoBehaviour {
 
-	
+	public bool isOpen;
+	public GameObject uiGroup;
+
+	private void Update () {
+		if(Input.GetButtonDown("Inventory"))
+		{
+			isOpen = !isOpen;
+		}
+
+		if(!isOpen)
+		{
+			uiGroup.SetActive(false);
+
+			return;
+		}
+
+		uiGroup.SetActive(true);
+	}
 }
