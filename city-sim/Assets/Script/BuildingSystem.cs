ed0c3105a8ed0d7f05447459c5ee7a11d5842232
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-03-21 21:32:42 +0100

    simple building system

---
+++ city-sim/Assets/Script/BuildingSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSystem : MonoBehaviour
+{
+    public Transform ghostObj;
+    public GameObject obj;
+
+    int lastPosX, lastPosY, lastPosZ;
+    Vector3 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            int posX = (int)Mathf.Round(hit.point.x);
+            int posZ = (int)Mathf.Round(hit.point.z);
+
+            if(posX != lastPosX || posZ != lastPosZ)
+            {
+                lastPosX = posX;
+                lastPosZ = posZ;
+
+                ghostObj.position = new Vector3(posX, 0.0f, posZ);
+            }
+
+            if(Input.GetButtonDown("Fire1"))
+            {
+                Instantiate(obj, ghostObj.position, Quaternion.identity);
+            }
+        }
+    }
+}
