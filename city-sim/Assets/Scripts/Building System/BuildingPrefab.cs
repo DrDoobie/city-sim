4734c89da9f5b3f857ab2aa4efd33f5cf6c1b46e
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-03-21 16:19:10 +0100

    delete old proj

--- city-sim/Assets/Scripts/Building System/BuildingPrefab.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BuildingPrefab : MonoBehaviour {
-
-	public bool placeable, canAfford;
-	public int prefab;
-}
