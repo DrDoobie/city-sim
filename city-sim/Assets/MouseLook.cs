62fb966d77904c4a3c84030e4ead33f0b5fdcdd7
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-03-21 17:46:08 +0100

    basic player movement

--- city-sim/Assets/MouseLook.cs
+++ city-sim/Assets/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSens = 100.0f;
+    public float mouseSens = 20.0f;
     public Transform playerBod;
     private float xRotation = 0.0f;
 
@@ -17,13 +17,18 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * (mouseSens * Time.deltaTime);
-        float mouseY = Input.GetAxis("Mouse Y") * (mouseSens * Time.deltaTime);
+        LookController();
+    }
+
+    private void LookController ()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * ((mouseSens * 10.0f) * Time.deltaTime);
+        float mouseY = Input.GetAxis("Mouse Y") * ((mouseSens * 10.0f) * Time.deltaTime);
 
         //Future reference, this is where to invert mouse y
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
-    
+
         transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
         playerBod.Rotate(Vector3.up * mouseX);
     }
