d28dd7dbb7ab405d1b7a5f58f36d84d8a9cf2e8f
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-03-21 18:58:33 +0100

    fixed audio listener bug

--- city-sim/Assets/Script/GameController.cs
+++ city-sim/Assets/Script/GameController.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public bool rtsMode;
+    public bool rtsMode = true;
     public Camera rtsCam, fpsCam;
     public Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rtsMode = true;
+        fpsCam.GetComponent<AudioListener>().enabled = false;
     }
 
     // Update is called once per frame
@@ -33,14 +34,22 @@ public class GameController : MonoBehaviour
 
             fpsCam.GetComponent<Camera>().enabled = false;
 
+            rtsCam.GetComponent<AudioListener>().enabled = true;
+
+            fpsCam.GetComponent<AudioListener>().enabled = false;
+
             player.GetComponent<PlayerMovement>().enabled = false;
 
             return;
         }
 
         rtsCam.GetComponent<Camera>().enabled = false;
-        
+
         fpsCam.GetComponent<Camera>().enabled = true;
+
+        rtsCam.GetComponent<AudioListener>().enabled = false;
+
+        fpsCam.GetComponent<AudioListener>().enabled = true;
 
         player.GetComponent<PlayerMovement>().enabled = true;
     }
