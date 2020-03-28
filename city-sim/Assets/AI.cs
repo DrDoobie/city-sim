a3c28b669f114302e597f9543fe2b780e5ea6254
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-03-29 00:50:28 +0100

    added basic random wandering ai

---
+++ city-sim/Assets/AI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public float time;
+    public NavMeshAgent agent;
+    public Transform target;
+
+    float _time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _time = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if(time <= 0)
+        {
+            Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f)) + transform.position;
+
+            agent.SetDestination(position);
+
+            time = _time;
+        }
+    }
+}
