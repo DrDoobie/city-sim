2d5218acc509b93fe8e0155037a4434fa18bdcd4
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-05 19:16:39 +0200

    switched to linear colourspace

---
+++ city-sim/Assets/Scripts/AI/HumanAI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanAI : MonoBehaviour
+{
+    void Start()
+    {
+        GameController.Instance.resourceController.population++;
+    }
+}
--- city-sim/Assets/Scripts/Home.cs
+++ city-sim/Assets/Scripts/Home.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class Home : MonoBehaviour
 {
+    public int maxOccupants = 1, occupants = 0;
+    public GameObject occupant;
+
     void Start()
     {
         GameController.Instance.resourceController.homes++;
+    }
+
+    void Update()
+    {
+        if(occupants < maxOccupants)
+        {
+            SpawnOccupant();
+        }
+    }
+
+    void SpawnOccupant()
+    {
+        Instantiate(occupant, occupant.transform.position, Quaternion.identity);
+
+        occupants++;
     }
 }
