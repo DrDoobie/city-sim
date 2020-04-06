dfc9b64bd099486ae1d86552ebf585b8e4016b99
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-06 23:56:02 +0200

    starting to rewrite ai completely

---
+++ city-sim/Assets/Animal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Animal : MonoBehaviour
+{
+    public Animator animator;
+    public NavMashAgent agent;
+
+    bool isAnimated = false;
+
+    void Start(){
+        if(animator != null)
+        {
+            isAnimated = true;
+        }
+    }
+
+    void Update()
+    {
+        //Idle
+
+        //Wander
+
+        //Run
+
+        //Attack
+    }
+}
