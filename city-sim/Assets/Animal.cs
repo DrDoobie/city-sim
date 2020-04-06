94eb7785011328c5a31232365077cd524923eca2
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-07 00:10:16 +0200

    basic wander

--- city-sim/Assets/Animal.cs
+++ city-sim/Assets/Animal.cs
@@ -3,26 +3,57 @@ using UnityEngine.AI;
 
 public class Animal : MonoBehaviour
 {
+    public bool idle;
+    public float wanderCoolDown, wanderRadius;
+    public Vector3 destination;
     public Animator animator;
-    public NavMashAgent agent;
+    public NavMeshAgent agent;
 
     bool isAnimated = false;
+    float _wanderCoolDown;
 
     void Start(){
         if(animator != null)
         {
             isAnimated = true;
         }
+
+        Idle();
+
+        _wanderCoolDown = wanderCoolDown;
     }
 
     void Update()
     {
-        //Idle
+        agent.SetDestination(destination);
 
-        //Wander
+        Wander();
 
-        //Run
+        //Run away
 
         //Attack
+    }
+
+    void Idle()
+    {
+        Debug.Log("Idle");
+        idle = true;
+    }
+
+    void Wander()
+    {
+        idle = false;
+
+        wanderCoolDown -= Time.deltaTime;
+
+        if(wanderCoolDown <= 0.0f)
+        {
+            Vector3 wanderPosition = new Vector3(Random.Range(-wanderRadius, wanderRadius), 0, Random.Range(-wanderRadius, wanderRadius)) + transform.position; 
+
+            Debug.Log("Walking");
+            destination = wanderPosition;
+
+            wanderCoolDown = _wanderCoolDown;
+        }
     }
 }
