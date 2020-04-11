b30077b7bc8c3592473d23cec5d1ec7aa71dd461
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-11 17:31:41 +0200

    rewriting build controller

---
+++ city-sim/Assets/BuildingSystem2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSystem2 : MonoBehaviour
+{
+    public Camera rtsCam, fpsCam;
+
+    [Header("Build Ghost")]
+    public GameObject buildGhost;
+    int lastPosGhostX, lastPosGhostY, lastPosGhostZ;
+
+    Vector3 mousePosition;
+
+    void Update()
+    {
+        if(GameController.Instance.buildMode)
+        {
+            BuildController();
+        }
+    }
+
+    void BuildController()
+    {
+        RTSBuild();
+    }
+
+    void RTSBuild()
+    {
+        BuildGhost();
+    }
+
+    void BuildGhost()
+    {
+        mousePosition = Input.mousePosition;
+
+        Ray ray = rtsCam.ScreenPointToRay(mousePosition);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            //buildGhost.transform.position = hit.point;
+
+            //Grid movement
+            int posX = (int)Mathf.Round(hit.point.x);
+            int posZ = (int)Mathf.Round(hit.point.z);
+
+            if (posX != lastPosGhostX || posZ != lastPosGhostZ)
+            {
+                lastPosGhostX = posX;
+                lastPosGhostZ = posZ;
+
+                buildGhost.transform.position = new Vector3(posX, 0.0f, posZ);
+            }
+        }
+    }
+}
---
+++ city-sim/Assets/FarmPlot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FarmPlot : MonoBehaviour
+{
+    //Make it so you can place seeds on plot
+
+    //Make moisture system
+}
