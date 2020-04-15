54e17cafc927e35ea8fbb14b4a041fec335cdb5e
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-16 00:33:26 +0200

    draggable ui panels

---
+++ city-sim/Assets/UIDrag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIDrag : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    public RectTransform dragTransform;
+    //public Canvas canvas;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragTransform.anchoredPosition += eventData.delta;
+        //dragTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragTransform.SetAsLastSibling();
+    }
+}
