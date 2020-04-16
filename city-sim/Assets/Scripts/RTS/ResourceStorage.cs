733331b68d5bb71a9001f6e56ec067864f02e6a2
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-16 16:36:47 +0200

    buffed trees

--- city-sim/Assets/Scripts/RTS/ResourceStorage.cs
+++ city-sim/Assets/Scripts/RTS/ResourceStorage.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class ResourceStorage : MonoBehaviour
 {
     public int storageValue;
+    public string resourceType;
 
     public void AddToStorage()
     {
-        GameController.Instance.resourceController.maxWood += storageValue;
+        if(resourceType == "Wood")
+            GameController.Instance.resourceController.maxWood += storageValue;
+          
+        if(resourceType == "Stone")
+            GameController.Instance.resourceController.maxStone += storageValue;
     }
 }
