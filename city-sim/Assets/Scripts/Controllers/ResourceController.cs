5cad5d123c4e9373c31ca201df404fb39cad9957
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-04-16 15:17:37 +0200

    added resource caps

--- city-sim/Assets/Scripts/Controllers/ResourceController.cs
+++ city-sim/Assets/Scripts/Controllers/ResourceController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ResourceController : MonoBehaviour
 {
     public bool starving;
-    public int stone, wood;
+    public int stone, maxStone, wood, maxWood;
     public int population = 0, totalWorkers, avaliableWorkers, homes = 0;
     public float food, maxFood, foodLossRate;
     public Text stoneText, woodText, homesText, populationText, foodText;
@@ -19,25 +19,34 @@ public class ResourceController : MonoBehaviour
     void Update ()
     {
         UIController();
+        ResourceCap();
         FoodController();
     }
 
     void UIController()
     {
-        stoneText.text = "Stone: " + stone.ToString("0#");
-        woodText.text = "Wood: " + wood.ToString("0#");
+        stoneText.text = "Stone: " + stone.ToString("0#")  + "/" + maxStone.ToString("0#");
+        woodText.text = "Wood: " + wood.ToString("0#")  + "/" + maxWood.ToString("0#");
+        foodText.text = "Food: " + food.ToString("0#") + "/" + maxFood.ToString("0#");
+
         homesText.text = "Homes: " + homes.ToString("0#");
         populationText.text = "Population: " + population.ToString("0#");
-        foodText.text = "Food: " + food.ToString("0#") + "/" + maxFood.ToString("0#");
+    }
+
+    void ResourceCap()
+    {
+        if(food >= maxFood)
+            food = maxFood;
+
+        if(wood >= maxWood)
+            wood = maxWood;
+
+        if(stone >= maxStone)
+            stone = maxStone;
     }
 
     void FoodController()
     {
-        if(food >= maxFood)
-        {
-            food = maxFood;
-        }
-
         if(food <= 0)
         {
             starving = true;
