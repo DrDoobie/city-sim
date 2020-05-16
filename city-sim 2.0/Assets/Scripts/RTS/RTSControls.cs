1564f5be55b784e25fe59b81ae92cdc5b608a3a3
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-05-16 18:39:16 +0200

    animated character and cleanup

--- city-sim 2.0/Assets/Scripts/RTS/RTSControls.cs
+++ city-sim 2.0/Assets/Scripts/RTS/RTSControls.cs
@@ -7,9 +7,33 @@ public class RTSControls : MonoBehaviour
 {
     public Camera cam;
 
+    [Header("Animation")]
+    public bool isAnimated;
+    public Animator animator;
+
     void Update()
     {
         Controller();
+
+        if(isAnimated)
+        {
+            Animation();
+        }
+    }
+
+    void Animation()
+    {
+        if(GameController.Instance.playerAgent.remainingDistance > GameController.Instance.playerAgent.stoppingDistance)
+        {
+            //Moving
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isWalking", true);
+
+        } else {
+            //Not moving
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isWalking", false);
+        }
     }
 
     void Controller()
