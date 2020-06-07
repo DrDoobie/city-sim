139b74a3c3928d582cf62b14bac5f82b40746c48
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2020-06-07 18:14:52 +0200

    work on inventory system

--- city-sim 2.0/Assets/Scriptable Objects/Inventory/Scripts/InventoryObj.cs
+++ city-sim 2.0/Assets/Scriptable Objects/Inventory/Scripts/InventoryObj.cs
@@ -1,45 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
 
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
-public class InventoryObj : ScriptableObject
+public class InventoryObj : ScriptableObject, ISerializationCallbackReceiver
 {
+    public string savePath;
+    public ItemDatabaseObject database;
     public List<InventorySlot> container = new List<InventorySlot>();
 
     public void AddItem(ItemObj _item, int _amount)
     {
-        bool hasItem = false;
-
         for(int i = 0; i < container.Count; i++)
         {
             if(container[i].item == _item)
             {
                 container[i].AddAmount(_amount);
 
-                hasItem = true;
-
-                break;
+                return;
             }
         }
 
-        if(!hasItem)
+        container.Add(new InventorySlot(database.getID[_item], _item, _amount));
+    }
+
+    public void Save()
+    {
+        //Binary formatter in jason utility
+
+        //Serialize scriptable object into a string
+
+        //Binary formatter in file stream to create file and save stream in given locatoin
+
+        string saveData = JsonUtility.ToJson(this, true);
+
+        
+    }
+
+    public void Load()
+    {
+        //Load inventory
+    }
+
+    public void OnAfterDeserialize()
+    {
+        for(int i = 0; i < container.Count; i++)
         {
-            container.Add(new InventorySlot(_item, _amount));
+            container[i].item = database.getItem[container[i].id];
         }
     }
+
+    public void OnBeforeSerialize()
+    {
+    }   
 }
 
 [System.Serializable]
 public class InventorySlot
 {
+    public int id;
     public ItemObj item;
     public int amount;
 
-    public InventorySlot(ItemObj _item, int _amount)
+    public InventorySlot(int _id, ItemObj _item, int _amount)
     {
+        id = _id;
         item = _item;
-
         amount = _amount;
     }
 
---
+++ city-sim 2.0/Assets/Scriptable Objects/Items/Scripts/ItemDatabaseObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Database", menuName = "Inventory System/Items/Database")]
+public class ItemDatabaseObject : ScriptableObject, ISerializationCallbackReceiver
+{
+    public ItemObj[] items;
+    public Dictionary<ItemObj, int> getID = new Dictionary<ItemObj, int>();
+    public Dictionary<int, ItemObj> getItem = new Dictionary<int, ItemObj>();
+
+    public void OnAfterDeserialize()
+    {
+        getID = new Dictionary<ItemObj, int>();
+        getItem = new Dictionary<int, ItemObj>();
+
+        for(int i = 0; i < items.Length; i++)
+        {
+            getID.Add(items[i], i);
+            getItem.Add(i, items[i]);
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+    }   
+}
